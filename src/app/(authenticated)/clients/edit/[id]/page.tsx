1c487d4d83db095c738338843027ada790282959
James Latten <james.latten@phntm.com>
2025-04-19 18:11:52 -0400

    Testing new way of using props

--- src/app/(authenticated)/clients/edit/[id]/page.tsx
+++ src/app/(authenticated)/clients/edit/[id]/page.tsx
@@ -1,12 +1,18 @@
 import { Metadata } from "next";
 import EditClientPage from "./components/EditClientPage";
 
+interface ClientEditPageProps {
+    params: {
+        id: string;
+    };
+}
+
 export const metadata = {
     title: "Edit Client | " + process.env.NEXT_PUBLIC_APP_NAME,
     description: "Edit an existing client",
 };
 
-export default function ClientEditPage({ params }: { params: { id: string } }) {
+export default function ClientEditPage({ params }: ClientEditPageProps) {
     const clientId = params.id;
 
     return <EditClientPage clientId={clientId} />;
