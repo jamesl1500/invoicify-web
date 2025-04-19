87a0aec9c54d62a9e93f5503da4e97fedb91c866
James Latten <james.latten@phntm.com>
2025-04-19 18:23:01 -0400

    test

--- src/app/(authenticated)/clients/edit/[id]/page.tsx
+++ src/app/(authenticated)/clients/edit/[id]/page.tsx
@@ -1,19 +1,13 @@
 import { Metadata } from "next";
 import EditClientPage from "./components/EditClientPage";
 
-interface ClientEditPageProps {
-    params: {
-        id: string;
-    };
-}
-
 export const metadata = {
     title: "Edit Client | " + process.env.NEXT_PUBLIC_APP_NAME,
     description: "Edit an existing client",
 };
 
-const ClientEditPage = async ({ params }: ClientEditPageProps) => {
-    const clientId = params.id;
+const ClientEditPage = async (props: {params: Promise<{ id: string }>;}) => {
+    const clientId = await props.params.id;
 
     return <EditClientPage clientId={clientId} />;
 }
